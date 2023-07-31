b7275ec7d3cc63cd4f54d201fd596772120b24f2
VictorVechi <victorhvechi@gmail.com>
2023-07-31 09:23:54 -0300

    Criação do arquivo de população do Estoque

---
+++ população do banco/populaçãoEstoque.sql
@@ -0,0 +1,13 @@
+insert into Biblioteca.estoque value (null, 1, 16);
+insert into Biblioteca.estoque value (null, 2, 2);
+insert into Biblioteca.estoque value (null, 3, 8);
+insert into Biblioteca.estoque value (null, 4, 20);
+insert into Biblioteca.estoque value (null, 5, 13);
+insert into Biblioteca.estoque value (null, 6, 16);
+insert into Biblioteca.estoque value (null, 7, 9);
+insert into Biblioteca.estoque value (null, 8, 10);
+insert into Biblioteca.estoque value (null, 9, 15);
+insert into Biblioteca.estoque value (null, 10, 17);
+insert into Biblioteca.estoque value (null, 11, 12);
+
+select * from Biblioteca.estoque;
