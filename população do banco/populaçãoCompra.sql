f2d55ef65501573bea429e29b2d866210008dc94
VictorVechi <victorhvechi@gmail.com>
2023-07-31 09:24:03 -0300

    Criação do arquivo de população das Compras

---
+++ população do banco/populaçãoCompra.sql
@@ -0,0 +1,7 @@
+insert into Biblioteca.compra value (null, 2, 5, '2023-07-29');
+insert into Biblioteca.compra value (null, 1, 2, '2023-02-11');
+insert into Biblioteca.compra value (null, 3, 10, '2023-12-21');
+insert into Biblioteca.compra value (null, 4, 9, '2023-10-28');
+insert into Biblioteca.compra value (null, 5, 8, '2022-06-03');
+
+select * from Biblioteca.compra;
