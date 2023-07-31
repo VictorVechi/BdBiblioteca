b936fef6dc9b709d6267861146ed7a4e3572a8f8
VictorVechi <victorhvechi@gmail.com>
2023-07-31 09:23:46 -0300

    Criação do arquivo de população dos Livros

---
+++ população do banco/populaçãoLivro.sql
@@ -0,0 +1,13 @@
+insert into Biblioteca.livro value (null, 'Romeu e Julieta','1597-01-01', 5);
+insert into Biblioteca.livro value (null, 'O Hobbit','1937-09-21', 4);
+insert into Biblioteca.livro value (null, 'Dom casmurro','1899-12-05', 1);
+insert into Biblioteca.livro value (null, 'Senhora','1875-10-12', 2);
+insert into Biblioteca.livro value (null, 'H.P Pedra Filosofal','1997-06-26', 3);
+insert into Biblioteca.livro value (null, 'H.P e a Câmara secreta','1998-07-02', 3);
+insert into Biblioteca.livro value (null, 'H.P e o Prisioneiro de Azkaban','1999-07-08', 3);
+insert into Biblioteca.livro value (null, 'H.P e o Cálice de Fogo','2000-07-08', 3);
+insert into Biblioteca.livro value (null, 'H.P e a Ordem da Fênix','2003-06-21', 3);
+insert into Biblioteca.livro value (null, 'H.P e o Enigma do Príncipe','2005-07-16', 3);
+insert into Biblioteca.livro value (null, 'H.P e as Relíquias da Morte','2007-07-21', 3);
+
+select * from Biblioteca.livro;
