8621f180bd9bfc9fdbba500006f0de4206db3dc3
VictorVechi <victorhvechi@gmail.com>
2023-07-31 09:23:07 -0300

    Criação do arquivo de população geral do banco

---
+++ população do banco/populaçãoGeral.sql
@@ -0,0 +1,41 @@
+insert into Biblioteca.cliente value (null ,'kamily', 'kamykamy@g.com', '12345678910','12345678','+5541999999999');
+insert into Biblioteca.cliente value (null ,'Victor', 'victorvictor@g.com', '10987654321','12345678','+5541999999999');
+insert into Biblioteca.cliente value (null ,'Leonardo', 'leoleo@g.com', '11111111111','12345678','+5541999999999');
+insert into Biblioteca.cliente value (null ,'Igor', 'igorigor@g.com', '12121212121','12345678','+5541999999999');
+insert into Biblioteca.cliente value (null ,'Tufão', 'tufatufa@g.com', '23232323232','12345678','+5541999999999');
+
+insert into Biblioteca.escritor value (null, 'Machado de Assis');
+insert into Biblioteca.escritor value (null, 'José de Alencar');
+insert into Biblioteca.escritor value (null, 'JK. Rowling');
+insert into Biblioteca.escritor value (null, 'J.R.R Tolkien');
+insert into Biblioteca.escritor value (null, 'William Shakespeare');
+
+insert into Biblioteca.livro value (null, 'Romeu e Julieta','1597-01-01', 5);
+insert into Biblioteca.livro value (null, 'O Hobbit','1937-09-21', 4);
+insert into Biblioteca.livro value (null, 'Dom casmurro','1899-12-05', 1);
+insert into Biblioteca.livro value (null, 'Senhora','1875-10-12', 2);
+insert into Biblioteca.livro value (null, 'H.P Pedra Filosofal','1997-06-26', 3);
+insert into Biblioteca.livro value (null, 'H.P e a Câmara secreta','1998-07-02', 3);
+insert into Biblioteca.livro value (null, 'H.P e o Prisioneiro de Azkaban','1999-07-08', 3);
+insert into Biblioteca.livro value (null, 'H.P e o Cálice de Fogo','2000-07-08', 3);
+insert into Biblioteca.livro value (null, 'H.P e a Ordem da Fênix','2003-06-21', 3);
+insert into Biblioteca.livro value (null, 'H.P e o Enigma do Príncipe','2005-07-16', 3);
+insert into Biblioteca.livro value (null, 'H.P e as Relíquias da Morte','2007-07-21', 3);
+
+insert into Biblioteca.estoque value (null, 1, 16);
+insert into Biblioteca.estoque value (null, 2, 2);
+insert into Biblioteca.estoque value (null, 3, 8);
+insert into Biblioteca.estoque value (null, 4, 20);
+insert into Biblioteca.estoque value (null, 5, 13);
+insert into Biblioteca.estoque value (null, 6, 16);
+insert into Biblioteca.estoque value (null, 7, 9);
+insert into Biblioteca.estoque value (null, 8, 10);
+insert into Biblioteca.estoque value (null, 9, 15);
+insert into Biblioteca.estoque value (null, 10, 17);
+insert into Biblioteca.estoque value (null, 11, 12);
+
+insert into Biblioteca.compra value (null, 2, 5, '2023-07-29');
+insert into Biblioteca.compra value (null, 1, 2, '2023-02-11');
+insert into Biblioteca.compra value (null, 3, 10, '2023-12-21');
+insert into Biblioteca.compra value (null, 4, 9, '2023-10-28');
+insert into Biblioteca.compra value (null, 5, 8, '2022-06-03');
