82a1d5575b2f16dd3bc3a6691034a6ef93392db3
VictorVechi <victorhvechi@gmail.com>
2023-07-31 09:23:29 -0300

    Criação do arquivo de população dos Clientes

---
+++ população do banco/populaçãoCliente.sql
@@ -0,0 +1,6 @@
+insert into Biblioteca.cliente value (null ,'kamily', 'kamykamy@g.com', '12345678910','12345678','+5541999999999');
+insert into Biblioteca.cliente value (null ,'Victor', 'victorvictor@g.com', '10987654321','12345678','+5541999999999');
+insert into Biblioteca.cliente value (null ,'Leonardo', 'leoleo@g.com', '11111111111','12345678','+5541999999999');
+insert into Biblioteca.cliente value (null ,'Igor', 'igorigor@g.com', '12121212121','12345678','+5541999999999');
+insert into Biblioteca.cliente value (null ,'Tufão', 'tufatufa@g.com', '23232323232','12345678','+5541999999999');
+select * from Biblioteca.cliente;
