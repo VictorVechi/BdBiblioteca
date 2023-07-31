fcb0d43da69903e65a2addad1e6e9567507c53ac
VictorVechi <victorhvechi@gmail.com>
2023-07-31 09:23:37 -0300

    Criação do arquivo de população dos Escritores

---
+++ população do banco/populaçãoEscritor.sql
@@ -0,0 +1,7 @@
+insert into Biblioteca.escritor value (null, 'Machado de Assis');
+insert into Biblioteca.escritor value (null, 'José de Alencar');
+insert into Biblioteca.escritor value (null, 'JK. Rowling');
+insert into Biblioteca.escritor value (null, 'J.R.R Tolkien');
+insert into Biblioteca.escritor value (null, 'William Shakespeare');
+
+select * from Biblioteca.escritor;
